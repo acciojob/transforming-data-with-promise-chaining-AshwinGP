e5bd75d0fe655d666c483f8bbeda9570ceca054d
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-27 16:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
 //your JS code here. If required.
+
+var op=document.getElementById("output");
+var btn=document.getElementById("btn");
+function promise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+			var ip=document.getElementById("ip").value;
+            resolve(ip);
+        }, 2000);
+    });
+}
+
+function promise2(res) {
+    return new Promise((resolve, reject) => { 
+              setTimeout(() => {
+                resolve(res);
+        }, 2000); 
+    });
+}
+
+function sub(mul){
+	return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+		   resolve(mul-3);
+	   },1000);
+	})
+}
+function div(mul){
+	return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+		   resolve(mul/2);
+	   },1000);
+	})
+}
+function add(mul){
+	return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+		   resolve(mul+10);
+	   },1000);
+	})
+}
+function final(mul){
+	return new Promise((resolve,reject)=>{
+		   resolve(mul);
+	});
+}
+
+function onClick() {
+   promise()
+	   .then((res)=>{
+		   var re=res;
+	   document.getElementById("output").innerHTML =res;
+	   return promise2(re);
+   })
+	.then((res)=>{
+	   document.getElementById("output").innerHTML =res;
+		return sub(res);
+   }).then((res)=>{
+		document.getElementById("output").innerHTML =res;
+		return div(res);
+   }).then((res)=>{
+		document.getElementById("output").innerHTML =res;
+		return add(res)
+   }).then((res)=>{
+		document.getElementById("output").innerHTML =res;
+		return final(res);
+   }).then((res)=>{
+		document.getElementById("output").innerHTML ="Final Result:"+" "+res;
+	})
+	   
+}
+  
+
+
+btn.addEventListener('click', (event) => {
+    // event.preventDefault();
+    onClick();
+});
